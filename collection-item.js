6ed25da04b05b3a3587a89a84df66f68cecef39d
WFH Brian <brian@plexm.com>
2023-11-24 20:40:09 -0500

    up

--- collection-item.js
+++ collection-item.js
@@ -1,8 +1,7 @@
+const { AJSON } = require('./AJSON');
 const { Disk } = require('./Disk');
 const { CollectionItem } = require('./CollectionItem');
 const { Collection } = require('./Collection');
-
-// mock brain
 class Brain {
   constructor() {
     this.config = {};
@@ -21,3 +20,4 @@ exports.Brain = Brain;
 exports.Collection = Collection;
 exports.CollectionItem = CollectionItem;
 exports.Disk = Disk;
+exports.AJSON = AJSON;
