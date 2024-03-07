e622150d7c1196247f43d0c51bcaa96e5bc8b105
WFH Brian <brian@plexm.com>
2024-03-07 15:04:03 -0500

    Refactor file saving logic to improve performance

--- ObsAJSON.js
+++ ObsAJSON.js
@@ -54,40 +54,49 @@ class ObsAJSON extends LongTermMemory {
     const temp_file_path = this.file_path.replace('.ajson', '.temp.ajson');
     if(await this.adapter.exists(temp_file_path)) await this.adapter.remove(temp_file_path);
     try {
-      if(await this.adapter.exists(this.file_path)) await this.adapter.rename(this.file_path, temp_file_path);
       let file_content = [];
       const items = Object.values(this.items).filter(i => i.vec);
       const batches = Math.ceil(items.length / 1000);
-
       for(let i = 0; i < batches; i++) {
         file_content = items.slice(i * 1000, (i + 1) * 1000).map(i => i.ajson);
         const batch_content = file_content.join(",");
-        // if(i > 0) await this.adapter.append(this.file_path, ",");
-        if(i > 0) await this.adapter.append(this.file_path, ",\n");
-        await this.adapter.append(this.file_path, batch_content);
+        if(i > 0) await this.adapter.append(temp_file_path, ",\n");
+        await this.adapter.append(temp_file_path, batch_content);
       }
       // append last batch
       if(items.length > batches * 1000) {
-        await this.adapter.append(this.file_path, ",");
-        await this.adapter.append(this.file_path, items.slice(batches * 1000).map(i => i.ajson).join(","));
+        await this.adapter.append(temp_file_path, ",\n");
+        await this.adapter.append(temp_file_path, items.slice(batches * 1000).map(i => i.ajson).join(","));
       }
-
       const end = Date.now(); // log time
       const time = end - start;
-      console.log("Saved " + this.file_name + " in " + time + "ms");
+      if(force || await this.validate_save(temp_file_path, this.file_path)) {
+        if(await this.adapter.exists(this.file_path)) await this.adapter.remove(this.file_path);
+        await this.adapter.rename(temp_file_path, this.file_path);
+        console.log("Saved " + this.file_name + " in " + time + "ms");
+      }else{
+        console.log("Not saving " + this.file_name + " because new file is less than 50% of old file");
+      }
     } catch (err) {
       console.error("Error saving: " + this.file_name);
       console.error(err.stack);
       // set new file to "failed" and rename to inlclude datetime
-      const failed_file_path = this.file_path.replace('.ajson', '-' + Date.now() + '.failed.ajson');
-      // move temp file back if new file fails to save
-      await this.adapter.rename(this.file_path, failed_file_path);
-      await this.adapter.rename(temp_file_path, this.file_path);
+      const failed_file_path = temp_file_path.replace('.temp.', '.failed-' + Date.now() + '.');
+      await this.adapter.rename(temp_file_path, failed_file_path);
     }
     this._saving = false;
     // remove temp file after new file is saved
     if(await this.adapter.exists(temp_file_path) && await this.adapter.exists(this.file_path)) await this.adapter.remove(temp_file_path);
   }
+  async validate_save(new_file_path, old_file_path) {
+    const new_file_size = (await this.adapter.stat(new_file_path))?.size;
+    const old_file_size = (await this.adapter.stat(old_file_path))?.size;
+    if(!old_file_size) return true;
+    console.log("New file size: " + new_file_size + " bytes");
+    console.log("Old file size: " + old_file_size + " bytes");
+    return new_file_size > (old_file_size * 0.5);
+  }
+
   get file_name() { return super.file_name + '.ajson'; }
 }
 
